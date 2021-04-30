c7301610bd94c1e24e387c2320448ebb8aff7e90
jeromekasparian <jk@kasparian.eu>
2021-04-30 21:46:39 +0200

    Build 6 - Meilleure gestion des autorisations - meilleure gestion de l'orientation

--- Vitesse/ViewController.swift
+++ Vitesse/ViewController.swift
@@ -6,17 +6,23 @@
 //
 
 /// feuille de route
-// afficher km/h
-// couleur / contraste
-// affichage inversé (selon la position ?)
-// do not disturb : alerte
+// Amortir l'affichage au début si l'incertitude sur la localisation est trop grande
+// cacher la statusbar
+// localisation EN
+// Localisation DE
+// autres localisations ?
+// icone non SF
+// se souvenir de l'unité d'une fois sur l'autre
+//Rafraîchir l’affichage de la vitesse quand on change l’unité
+//Redemander l’autorisation d’accéder à la localisation quand l’app redémarre après une mise en veille
+//Mettre un chiffre après la virgule si la vitesse est plus petite que 10
 
 import UIKit
 import CoreLocation
 import CoreMotion
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
     var locationManager: CLLocationManager! = CLLocationManager()
     var unite: Int = 1 // par défaut, km/h
     let textesUnites: [String] = ["m/s","km/h","mph"]
@@ -26,10 +32,17 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     var anciennePositionDressee : Bool = true
     var nouveauDresse : Bool = true
     
+    
+    //    var imagePasDeVitesse = NSTextAttachment()
+    //    var messagePasDeVitesse = NSMutableAttributedString(string: "Attente localisation")
+    
     let motionManager = CMMotionManager()
-
+    
     @IBOutlet weak var affichageVitesse: UILabel!
-    @IBOutlet weak var affichageUnite: UIButton!
+    @IBOutlet var affichageUnite: UIButton!
+    @IBOutlet var imageLocalisation: UIImageView!
+    @IBOutlet var imagePasLocalisation: UIImageView!
+    @IBOutlet var imageLocalisationPerdue: UIImageView!
     
     @IBAction func changeUnite () {
         unite = (unite + 1) % 3
@@ -38,85 +51,162 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     override func viewDidLoad() {
         locationManager.requestWhenInUseAuthorization()
-        if (CLLocationManager.locationServicesEnabled())
-        {
-            print("accès localisation ok")
-        locationManager.delegate = self
-//        locationManager.distanceFilter = kCLDistanceFilterNone
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.startUpdatingLocation();
-        }
+        gereDroitsLocalisation(origineViewDidLoad: true)
         
         motionManager.deviceMotionUpdateInterval = 1
         // Get attitude orientation
-        motionManager.startDeviceMotionUpdates(to: .main) { [self] (motion, error) in
-            let tangage = motion!.attitude.pitch * radiansEnDegres  // basculement vers l'avant
-            let roulis = motion!.attitude.roll * radiansEnDegres    // basculement vers le côté
-            let azimutInverse = motion!.attitude.yaw * radiansEnDegres    // Azimut, haut du téléphone vers le sud = 0
-            print(String(format:"orientation : %.2f, %.2f, %.2f", tangage, roulis, azimutInverse))
-            nouveauDresse = (abs(tangage) > inclinaisonMax) || (abs(roulis) > inclinaisonMax)
-            DispatchQueue.main.async{
-                if !(nouveauDresse == anciennePositionDressee) {
-                    self.affichageVitesse.flipX()
-                    self.affichageUnite.flipX()
-                    anciennePositionDressee = nouveauDresse
-                }
-            if nouveauDresse { // le téléphone est penché -> on affiche le texte en gris pour lecture directe
-                        affichageVitesse.textColor = .lightGray
-//                print("dressé")
-            }
-            else {  // le téléphone est à plat -> on affiche le texte en blanc pour réflexion sur le pare-brise
-                        affichageVitesse.textColor = .white
-//                print("à plat")
-            }
-            } // DispatchQueue.main.async
-        } //  motionManager.startDeviceMotionUpdates
+            motionManager.startDeviceMotionUpdates(to: .main, withHandler: gereOrientation) //{ (motion, error) in
+//            let tangage = motion!.attitude.pitch * radiansEnDegres  // basculement vers l'avant
+//            let roulis = motion!.attitude.roll * radiansEnDegres    // basculement vers le côté
+//            let azimutInverse = motion!.attitude.yaw * radiansEnDegres    // Azimut, haut du téléphone vers le sud = 0
+//            print(String(format:"orientation : %.2f, %.2f, %.2f", tangage, roulis, azimutInverse))
+//            //            nouveauDresse = (abs(roulis) > inclinaisonMax)
+//            nouveauDresse = (!(UIDevice.current.orientation.isLandscape) || (abs(roulis) > inclinaisonMax))
+//            DispatchQueue.main.async{
+//                if !(nouveauDresse == anciennePositionDressee) {
+//                    self.affichageVitesse.flipX()
+//                    self.affichageUnite.flipX()
+//                    anciennePositionDressee = nouveauDresse
+//                }
+//                if nouveauDresse { // le téléphone est penché -> on affiche le texte en gris pour lecture directe
+//                    affichageVitesse.textColor = .lightGray
+//                    //                print("dressé")
+//                }
+//                else {  // le téléphone est à plat -> on affiche le texte en blanc pour réflexion sur le pare-brise
+//                    affichageVitesse.textColor = .white
+//                    //                print("à plat")
+//                }
+//                affichageUnite.setTitle(textesUnites[unite], for: .normal) // = textesUnites[unite]
+//            } // DispatchQueue.main.async
+   //     } //  motionManager.startDeviceMotionUpdates
         
         UIApplication.shared.isIdleTimerDisabled = true
         
-        affichageUnite.setTitle(textesUnites[unite], for: .normal) // = textesUnites[unite]
         let alert = UIAlertController(title: "Pour votre sécurité", message: "activez le mode \"Ne pas déranger\" du téléphone avant de conduire", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in print("tata")}))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: {_ in print("Alerte NPD validée")}))
         DispatchQueue.main.async{
+            self.affichageUnite.setTitle(self.textesUnites[self.unite], for: .normal)
             self.present(alert, animated: true)
-            self.affichageVitesse.text = "Attente du signal…"
         }
         super.viewDidLoad()
         print("init ok")
     }
+    
+    func gereOrientation(motion:CMDeviceMotion?,error:Error?) {
+        let tangage = motion!.attitude.pitch * radiansEnDegres  // basculement vers l'avant
+        let roulis = motion!.attitude.roll * radiansEnDegres    // basculement vers le côté
+        let azimutInverse = motion!.attitude.yaw * radiansEnDegres    // Azimut, haut du téléphone vers le sud = 0
+        print(String(format:"orientation : %.2f, %.2f, %.2f", tangage, roulis, azimutInverse))
+        //            nouveauDresse = (abs(roulis) > inclinaisonMax)
+//        nouveauDresse = (!(UIDevice.current.orientation.isLandscape) || (abs(roulis) > inclinaisonMax))
+        nouveauDresse = ((abs(roulis) < abs(tangage)) || (abs(roulis) > inclinaisonMax))
+        DispatchQueue.main.async{
+            if !(self.nouveauDresse == self.anciennePositionDressee) {
+                self.affichageVitesse.flipX()
+                self.affichageUnite.flipX()
+                self.anciennePositionDressee = self.nouveauDresse
+            }
+            if self.nouveauDresse { // le téléphone est penché -> on affiche le texte en gris pour lecture directe
+                self.affichageVitesse.textColor = .lightGray
+                //                print("dressé")
+            }
+            else {  // le téléphone est à plat -> on affiche le texte en blanc pour réflexion sur le pare-brise
+                self.affichageVitesse.textColor = .white
+                //                print("à plat")
+            }
+            self.affichageUnite.setTitle(self.textesUnites[self.unite], for: .normal) // = textesUnites[unite]
+        } // DispatchQueue.main.async
+    }
+    
+    
+    func gereDroitsLocalisation(origineViewDidLoad : Bool) {
+        print("lancement au démarrage : \(origineViewDidLoad)")
+        locationManager.delegate = self
 
-    //CLLocationManagerDelegate
+        if (CLLocationManager.locationServicesEnabled()) { // la localisation est activée sur l'appareil
+            print("droits de localisation : ", CLLocationManager.authorizationStatus().rawValue)
+
+            switch CLLocationManager.authorizationStatus() {
+                case .authorizedAlways, .authorizedWhenInUse:  // l'app a l'autorisation d'accéder à la localisation
+                locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                locationManager.startUpdatingLocation();
+                print("acces localisation ok")
+                //            self.imagePasDeVitesse.image = UIImage(systemName: "location.fill")
+                DispatchQueue.main.async{
+                    self.imageLocalisation.isHidden = false
+                    self.imagePasLocalisation.isHidden = true
+                    self.imageLocalisationPerdue.isHidden = true
+                }
+            case .denied, .restricted:
+                print("acces localisation pas ok pour l'app")
+                DispatchQueue.main.async{
+                    self.imageLocalisation.isHidden = true
+                    self.imagePasLocalisation.isHidden = false
+                    self.imageLocalisationPerdue.isHidden = true
+                }
+            case .notDetermined:
+                print("not determined")
+            default:
+                print("défaut")
+            } // switch
+        }  //  if (CLLocationManager.locationServicesEnabled())
+        else {
+            print("acces localisation pas ok pour le téléphone")
+            //            print("􀘭")//accès localisation  pas ok")
+            DispatchQueue.main.async{
+                self.imageLocalisation.isHidden = true
+                self.imagePasLocalisation.isHidden = false
+                self.imageLocalisationPerdue.isHidden = true
+            }
+        }
+    }
+    
+    //    CLLocationManagerDelegate
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let location:CLLocation = locations.last!
-//        print("vitesse : \(location.speed) m/s")
+        //        print("vitesse : \(location.speed) m/s")
         let vitesse = location.speed * facteurUnites[unite]
-//        print("vitesse \(vitesse) km/h")
+        print("vitesse \(vitesse) km/h")
         DispatchQueue.main.async{
-            if vitesse >= 0 { self.affichageVitesse.text = String(format:"%.0f",vitesse) }
-            else { self.affichageVitesse.text = "Attente du signal…" }
-//        print("locations = \(String(describing: locations))")
+            self.imageLocalisation.isHidden = true
+            self.imagePasLocalisation.isHidden = true
+            if vitesse >= 0 {
+                self.imageLocalisationPerdue.isHidden = true
+                self.affichageVitesse.isHidden = false
+                self.affichageVitesse.text = String(format:"%.0f",vitesse)
+            }
+            else {
+                self.affichageVitesse.isHidden = false
+                self.affichageVitesse.text = ""
+                self.imageLocalisationPerdue.isHidden = false
+                print("pas de signal")
+            }
+            //        print("locations = \(String(describing: locations))")
         }
         
     }
-
+    
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print(error)
     }
     
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        gereDroitsLocalisation(origineViewDidLoad: false)
+    }
 
-
+    
     
 }
 
 extension UIView {
-
+    
     /// Flip view horizontally.
     func flipX() {
         transform = CGAffineTransform(scaleX: -transform.a, y: transform.d)
     }
-
+    
     /// Flip view vertically.
     func flipY() {
         transform = CGAffineTransform(scaleX: transform.a, y: -transform.d)
     }
- }
+}
