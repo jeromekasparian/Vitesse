4bcc715b6625b21361ff75851ffb5086ba18cf69
jeromekasparian <jk@kasparian.eu>
2022-09-18 10:36:06 +0200

    Build 97. Affichage de l'unité du dénivelé

--- Vitesse/StatsModalViewController.swift
+++ Vitesse/StatsModalViewController.swift
@@ -121,7 +121,8 @@ class StatsModalViewController: UIViewController {
             if (unite == 0) { labelDistanceTotaleSession.text = String(format: "%.0f ", distanceTotaleSession * facteurUnitesDistance[unite]) }
             else { labelDistanceTotaleSession.text = String(format: "%.1f ", distanceTotaleSession * facteurUnitesDistance[unite]) }
             labelDistanceTotaleSession.text?.append(textesUnitesDistance[unite])
-            labelDeniveleSession.text = String(format: "➚ %.0f m, ➘ %.0f m", denivelePositifSession, deniveleNegatifSession)
+            labelDeniveleSession.text = String(format: "➚ %.0f m, ➘ %.0f m", denivelePositifSession  * facteurUnitesDistance[unite], deniveleNegatifSession * facteurUnitesDistance[unite])
+            labelDeniveleSession.text?.append(textesUnitesDistance[unite])
             //            if (premierTempsValide == 0.0) || !debugMode {
             //                affichageTempsSession.isHidden = true
             //            }
